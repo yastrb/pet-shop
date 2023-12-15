1b13a32372070a1677a7f558628fd76f9c7f5041
Olena Yastrebtsova <uaalena202@gmail.com>
2023-12-15 12:30:08 +0200

    My first git commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import { StickyNavbar } from './Components/Navbar/Header';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <StickyNavbar></StickyNavbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Navbar/Logo.jsx
@@ -0,0 +1,10 @@
+﻿import logo from "../Assets/Images/Main-Logo.svg"
+export function DefaultImg() {
+    return (
+        <img
+            className="h-[100px]  object-cover object-center"
+            src={logo}
+            alt="logoimage"
+        />
+    );
+}
